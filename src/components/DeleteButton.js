a68b79b4899939db77220098d5ce50c605d11853
DESKTOP-G145FC7\moon <msw711666@naver.com>
2022-03-31 17:03:46 +0900

    add delete all button

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 
-const DeleteButton = ()=> {
+const DeleteButton = (props)=> {
     return (
         <TouchableOpacity>
-            <Text style={{ fontSize : 24, color : 'white', backgroundColor:'red' }}>
+            <Text 
+                style={{ 
+                    fontSize : 24, color : 'white', backgroundColor:'red',
+                    padding : 16, margin : 10, borderRadius : 8
+                }}
+                onPress={() => props.onPress()}
+            >
                 Delete All 
             </Text>
         </TouchableOpacity>
